a69080d0cad4ecc258385dbdf28d23c6b495755a
HacenMario <stevenhacen@gmail.com>
2026-07-20 13:57:50 +0100

    Add files via upload

---
+++ Student.js
@@ -0,0 +1,19 @@
+﻿const mongoose = require('mongoose');
+
+const StudentSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  isInside: {
+    type: Boolean,
+    default: false,
+  },
+  lastUpdate: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('Student', StudentSchema);
