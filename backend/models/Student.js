336ba3936ecca19a2134594cc1ad26c859e8fd82
HacenMario <stevenhacen@gmail.com>
2026-07-20 14:28:50 +0100

    Enhance Student model with additional fields

--- backend/models/Student.js
+++ backend/models/Student.js
@@ -1,10 +1,36 @@
-﻿const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
 const StudentSchema = new mongoose.Schema({
+  studentId: {
+    type: String,
+    unique: true,
+    required: true,
+  },
   name: {
     type: String,
     required: true,
     trim: true,
+  },
+  parentName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  address: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
   },
   isInside: {
     type: Boolean,
